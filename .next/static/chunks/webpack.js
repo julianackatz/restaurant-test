68abc93692bf648c3ff93d461f0bd5581374c1c5
Juliana Katz <julianalimacunha@hotmail.com>
2023-09-23 15:30:55 -0400

    revert changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7ae8d519a4c6e9e237d5";
+/******/ 	var hotCurrentHash = "daefa0f4f083c39cdd34";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
